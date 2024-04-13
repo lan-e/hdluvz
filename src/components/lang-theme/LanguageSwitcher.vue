2b146561b74e551c4a657b844ffb5a2b1a0b4f1b
lan-e <neladuranec@gmail.com>
2024-04-13 14:50:23 +0200

    replace manual colors with variables, fetch data

--- src/components/lang-theme/LanguageSwitcher.vue
+++ src/components/lang-theme/LanguageSwitcher.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="hello">
-    <div v-if="languages">
-      <span v-for="(lng, index) in Object.keys(languages)" :key="lng">
-        <a v-if="$i18next.resolvedLanguage !== lng" v-on:click="$i18next.changeLanguage(lng)">
-          {{ languages[lng].nativeName }}
-        </a>
-        <strong v-if="$i18next.resolvedLanguage === lng">
-          {{ languages[lng].nativeName }}
-        </strong>
-        <span v-if="index < (Object.keys(languages).length - 1)">&nbsp;|&nbsp;</span>
-      </span>
-    </div>
+  <div v-if="languages" class="switchlang">
+    <span v-for="(lng, index) in Object.keys(languages)" :key="lng">
+      <a v-if="$i18next.resolvedLanguage !== lng" v-on:click="$i18next.changeLanguage(lng)">
+        {{ languages[lng].nativeName }}
+      </a>
+      <strong v-if="$i18next.resolvedLanguage === lng">
+        {{ languages[lng].nativeName }}
+      </strong>
+      <span v-if="index < (Object.keys(languages).length - 1)">&nbsp;|&nbsp;</span>
+    </span>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,36 @@
       <div class="bgMask"></div>
       <div class="overlayText"><span class="red">HDLU</span> VARAŽDIN</div>
     </div>
+    <div class="posts">
+      <div v-for="post in posts" class="post">
+        <div class="row">
+          <img :src="post.author.avatar_URL" alt="">
+          <div class="name">{{ post.author.name }}</div>
+        </div>
+        <h3>{{ post.title }}</h3>
+        <div class="content" v-html="removeTags(post.content)"></div>
+      </div>
+    </div>
     <Gallery />
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import Gallery from '@/components/Gallery.vue';
+let posts = ref(null);
+
+async function fetchData() {
+  const data = await fetch("https://public-api.wordpress.com/rest/v1.1/sites/hdluvz.wordpress.com/posts/?number=2&pretty=true");
+  const response = await data.json();
+  posts.value = response.posts;
+}
+
+function removeTags(content) {
+  return content.replace(/<p>/g, '').replace(/<\/p>/g, '')
+}
+
+onMounted(() => {
+  fetchData()
+})
 </script>
