6414fb077a564bed778ec05d52f02e66af0415e9
lan-e <neladuranec@gmail.com>
2024-06-06 00:25:14 +0200

    working localhost rest api

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -2,14 +2,11 @@
     <div class="posts-container">
         <hr>
         <div class="posts">
-            <div v-for="post in posts" class="post">
-                <!-- <div class="row">
-                    <img :src="post.author.avatar_URL" alt="">
-                    <div class="name">{{ post.author.name }}</div>
-                </div> -->
-                <router-link to="/exh">
-                    <h3>{{ post.title }}</h3>
-                    <div class="content" v-html="removeTags(post.content)"></div>
+            <div v-for="post in posts" :key="post.value.id" class="post">
+                <router-link :to="`/exh/${post.value.id}`">
+                    <h3>{{ post.value.title.rendered }}</h3>
+                    <div class="content" v-html="post.value.content.rendered"></div>
+                    <img :src="post.value.acf.poster.link" alt="Poster Image">
                 </router-link>
             </div>
         </div>
@@ -18,17 +15,13 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-let posts = ref(null);
+let posts = ref([]);
 
 async function fetchData() {
-    const data = await fetch("https://public-api.wordpress.com/rest/v1.1/sites/hdluvz.wordpress.com/posts/?number=3&pretty=true");
+    // const data = await fetch("https://public-api.wordpress.com/rest/v1.1/sites/hdluvz.wordpress.com/posts/?number=3&pretty=true");
+    const data = await fetch("http://localhost/hdlu/wp-json/wp/v2/events/?number=3&pretty=true");
     const response = await data.json();
-    posts.value = response.posts;
-    console.log(posts.value)
-}
-
-function removeTags(content) {
-    return content.replace(/<p>/g, '').replace(/<\/p>/g, '')
+    posts.value = response.map(resp => ({ value: resp }));
 }
 
 onMounted(() => {
